a051c4717f57504ba0a126cfa843624866b145ec
Anton Nikolenko <nikolenkoanton92@gmail.com>
2015-11-04 18:46:17 -0800

    added skeleton

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-var request = require('request')
+var request = require('request');
 
 function Waitlisted(domain) {
-  this.domain = domain
+  this.domain = domain;
 }
 
-Waitlisted.prototype.create = function() {
+Waitlisted.prototype.create = function(email, name, cb) {
+  if (!email) {
+    throw new Error('Email should be specified');
+  }
 
-}
+  if (!name) {
+    throw new Error('Name should be specified');
+  }
 
-Waitlisted.prototype.get = function() {
+  request.post({
+    url: 'https://' + this.domain + '.app.waitlisted.co/api/v1/reservation',
+    form: {
+      email: email,
+      name: name
+    }
+  }, function(err, httpResponse, body) {
 
-}
+  });
+};
 
-module.exports = Waitlisted
+Waitlisted.prototype.get = function(email, cb) {
+
+  if (!email) {
+    throw new Error('Email should be specified');
+  }
+
+  request('https://' + this.domain + '.app.waitlisted.co/api/v1/reservation?email=' + email, function(err, httpResponse, body) {
+
+  });
+};
+
+module.exports = Waitlisted;
