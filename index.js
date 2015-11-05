95352d3b23cbe5eaf6a529392efbd512a680204e
Anton Nikolenko <nikolenkoanton92@gmail.com>
2015-11-04 17:54:36 -0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var request = require('request')
+
+function Waitlisted(domain) {
+  this.domain = domain
+}
+
+Waitlisted.prototype.create = function() {
+
+}
+
+Waitlisted.prototype.get = function() {
+
+}
+
+module.exports = Waitlisted
